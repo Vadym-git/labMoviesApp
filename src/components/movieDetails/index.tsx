1a2c3e4628bb4b30856471e3d79770c18a6d2171
Vadym <vadim.melnichenko@gmail.com>
2025-06-30 13:17:13 +0100

    Movie Details Page - included production countries.

--- src/components/movieDetails/index.tsx
+++ src/components/movieDetails/index.tsx
@@ -55,6 +55,20 @@ const MovieDetails: React.FC<MovieDetailsProps> = (movie) => {
         />
         <Chip label={`Released: ${movie.release_date}`} />
       </Paper>
+      <Paper component="ul" sx={styles.chipSet}>
+        <li>
+          <Chip
+            label="Production Countries"
+            sx={styles.chipLabel}
+            color="primary"
+          />
+        </li>
+        {movie.production_countries.map((g) => (
+          <li key={g.iso_3166_1}>
+            <Chip label={g.name} />
+          </li>
+        ))}
+      </Paper>
     </>
   );
 };
